4e24e0a64c345a134d8204c201f91f680102136e
Santiago <santiago.zibe@gmail.com>
2022-06-14 22:20:26 -0300

    axios

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,5 +1,5 @@
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
-
+import { pokemonApi } from '../../../api/pokemonApi'
 
 // funion que regresa otra funcion 
 export const getPokemons = (page = 0) => {
@@ -11,9 +11,11 @@ export const getPokemons = (page = 0) => {
           dispatch(startLoadingPokemons);
 
           // TODO Realizar peticion http
-          const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
-          const data = await resp.json();
-          console.log(data);
+          // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
+          // const data = await resp.json();
+          // console.log(data);
+
+          const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`)
 
           dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
      }
